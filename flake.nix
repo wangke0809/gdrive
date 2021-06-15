a06731c731225c25b88a9c9cef96aef942ea3c65
Harsh Shandilya <me@msfjarvis.dev>
2021-06-15 16:25:44 +0530

    feat(nix): set up development shell
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git
+            go-outline
+            go_1_19
+            gopls
+            gotools
+          ];
+        };
+      });
+}
